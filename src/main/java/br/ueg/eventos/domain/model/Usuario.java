88a4a9c84b836928f4da10857db6472e3ad23f71
Stephanie Christine <stephaniecsilva88@gmail.com>
2026-09-07 23:06:27 -0300

    padroes de email e senha usando Regex, metodos de acao para alterar atributos: nome, emmail e senha.

--- src/main/java/br/ueg/eventos/domain/model/Usuario.java
+++ src/main/java/br/ueg/eventos/domain/model/Usuario.java
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern;
+
 public class Usuario {
 
     //atributos
@@ -12,11 +14,42 @@ public class Usuario {
     //construtores
     public Usuario(Integer id, String nome, String email, String senha, String perfil, String tipo) {
         this.id = id;
-        this.nome = nome;
-        this.email = email;
+        alterarNome(nome);
+        alterarEmail(email);
         this.senha = senha;
         this.perfil = perfil;
         this.tipo = tipo;
     }
 
-    
+    //Métodos de ação
+    public void alterarNome(String novoNome) {
+        if (novoNome == null || novoNome.trim().isEmpty()) {
+            throw new IllegalArgumentException("O nome não pode estar em branco!");
+        }
+        this.nome = novoNome;
+    }
+
+    public void alterarEmail(String novoEmail) {
+        if (novoEmail == null || novoEmail.trim().isEmpty()) {
+            throw new IllegalArgumentException("O email não pode estar em branco!");
+        }
+
+        if (!EMAIL_PATTERN.matcher(novoEmail).matches()) {
+            throw new IllegalArgumentException("O formato do e-mail é inválido.");
+        }
+
+        this.email = novoEmail;
+    }
+
+    //Validações
+
+
+    //Getters (Não possui Setters, pois se não qualquer classe pode alterar os atributos do usuário)
+    public Integer getId() {return id;}
+    public String getNome() {return nome;}
+    public String getEmail() {return email;}
+    public String getSenha() {return senha;}
+    public String getPerfil() {return perfil;}
+    public String getTipo() {return tipo;}
+
+}
