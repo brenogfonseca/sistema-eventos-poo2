218340a2e2335a2d11efba85baa4062329283fdb
Stephanie Christine <stephaniecsilva88@gmail.com>
2026-09-07 23:57:31 -0300

    mudançado tipo dos atributos perfil e tipo de String para enum. adição de construtor com id em branco para a criação do objeto na base de dados (a base gera o id não a classe).

---
+++ src/main/java/br/ueg/eventos/domain/model/Perfil.java
@@ -0,0 +1,5 @@
+public enum Perfil {
+    ORGANIZADOR,
+    ALUNO,
+    PALESTRANTE;
+}
--- src/main/java/br/ueg/eventos/domain/model/Usuario.java
+++ src/main/java/br/ueg/eventos/domain/model/Usuario.java
@@ -2,24 +2,51 @@ import java.util.regex.Pattern;
 
 public class Usuario {
 
+    //Padrões e constantes
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@([A-Za-z0-9.-]+\\.[A-Za-z]{2,})$");
+    private static final Pattern SENHA_PATTERN = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&*]).{8,}$");
+
     //atributos
     private Integer id;
     private String nome;
     private String email;
     private String senha;
-    private String perfil;
-    private String tipo;
+    private Perfil perfil;
+    private TipoUsuario tipo;
 
 
-    //construtores
-    public Usuario(Integer id, String nome, String email, String senha, String perfil, String tipo) {
-        this.id = id;
+    //construtores:
+    // CONSTRUTOR: Para criar um usuário NOVO (O Java deixa o 'id' como null temporariamente, quem vai preencher é o banco de dados)
+    public Usuario(String nome, String email, String senha, Perfil perfil, TipoUsuario tipo) {
+        this.id = null; // O banco de dados vai gerar esse cara depois!
         alterarNome(nome);
         alterarEmail(email);
-        this.senha = senha;
+        alterarSenha(senha);
+        
+        // Validação simples apenas para garantir que não enviaram null
+        if (perfil == null) throw new IllegalArgumentException("O perfil não pode ser nulo!");
+        if (tipo == null) throw new IllegalArgumentException("O tipo não pode ser nulo!");
+        
         this.perfil = perfil;
         this.tipo = tipo;
     }
+
+
+    // CONSTRUTOR: Para buscas.
+    public Usuario(Integer id, String nome, String email, String senha, Perfil perfil, TipoUsuario tipo) {
+        this.id = id;
+        alterarNome(nome);
+        alterarEmail(email);
+        alterarSenha(senha);
+
+        // Validação simples apenas para garantir que não enviaram null
+        if (perfil == null) throw new IllegalArgumentException("O perfil não pode ser nulo!");
+        if (tipo == null) throw new IllegalArgumentException("O tipo não pode ser nulo!");
+
+        this.perfil = perfil;
+        this.tipo = tipo;
+    }
+
 
     //Métodos de ação
     public void alterarNome(String novoNome) {
@@ -41,7 +68,17 @@ public class Usuario {
         this.email = novoEmail;
     }
 
-    //Validações
+    public void alterarSenha(String novaSenha) {
+        if (novaSenha == null || novaSenha.trim().isEmpty()) {
+            throw new IllegalArgumentException("A senha não pode estar em branco!");
+        }
+
+        if (!SENHA_PATTERN.matcher(novaSenha).matches()) {
+            throw new IllegalArgumentException("A senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais.");
+        }
+
+        this.senha = novaSenha;
+    }
 
 
     //Getters (Não possui Setters, pois se não qualquer classe pode alterar os atributos do usuário)
@@ -49,7 +86,7 @@ public class Usuario {
     public String getNome() {return nome;}
     public String getEmail() {return email;}
     public String getSenha() {return senha;}
-    public String getPerfil() {return perfil;}
-    public String getTipo() {return tipo;}
+    public Perfil getPerfil() {return perfil;}
+    public TipoUsuario getTipo() {return tipo;}
 
 }
